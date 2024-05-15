6879c064c0c117ec4710f03681709a9e61057017
Scott Fennell <slfennell01@gmail.com>
2024-05-15 17:13:45 -0400

    Add files via upload
    Add output functionality to display bill, tip, and total

--- cc_1.js
+++ cc_1.js
@@ -9,4 +9,4 @@ const bills = [100];
 const tips = [tip(bills[0])];
 const totals = [bills[0] +tips[0]];
 
-console.log(bills, tips, totals);
+console.log("The bill was $", bills, ", the tip was $",tips, ", and the total cost was $", totals, ".");
