6eabca72caa3c01c527ece4c4365236230f33838
Scott Fennell <slfennell01@gmail.com>
2024-05-15 17:20:18 -0400

    Create and test calcTip function
    Create and test calcTip function

--- cc_1.js
+++ cc_1.js
@@ -2,11 +2,13 @@
 //CC_1 Steven's Tip Calculator
 
 /* 1.Determine the tip amount based on the bill using a 
-ternary operator instead of a if/esle statement. variable named tip.*/
+ternary operator instead of a if/esle statement. Function named calcTip.*/
 
-const tip = bill => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+const calcTip = bill => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+
+// Test witha bill value of 100
 const bills = [100];
-const tips = [tip(bills[0])];
+const tips = [calcTip(bills[0])];
 const totals = [bills[0] +tips[0]];
 
 console.log("The bill was $", bills, ", the tip was $",tips, ", and the total cost was $", totals, ".");
