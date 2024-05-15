d139609d0034bdde473c4ec9647931cecd9da220
Scott Fennell <slfennell01@gmail.com>
2024-05-15 17:10:14 -0400

    Add files via upload
    Initial logic for calculating tip using ternary operator

---
+++ cc_1.js
@@ -0,0 +1,12 @@
+// Scott Fennell (U86364709)
+//CC_1 Steven's Tip Calculator
+
+/* 1.Determine the tip amount based on the bill using a 
+ternary operator instead of a if/esle statement. variable named tip.*/
+
+const tip = bill => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+const bills = [100];
+const tips = [tip(bills[0])];
+const totals = [bills[0] +tips[0]];
+
+console.log(bills, tips, totals);
